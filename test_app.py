473d6375cfc5b57b05a44a82feafcc33bb236586
Austin Neuvy <aymericneuvy@Aymerics-Laptop.local>
2025-01-17 14:28:15 +0100

    Fix test issue

--- test_app.py
+++ test_app.py
@@ -2,5 +2,5 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5  # This will pass
-    assert add(6, 3) == 5  # Deliberate mistake to fail the test
+    assert add(6, 3) == 9  # Deliberate mistake to fail the test
     assert add(-1, 1) == 0  # This will pass
