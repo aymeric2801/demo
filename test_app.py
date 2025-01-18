401ebded7535672d9661d78fc4cba7423c286d7a
Austin Neuvy <aymericneuvy@aymerics-laptop.home>
2025-01-18 23:38:55 +0100

    introduce deliberate failing test

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5  # This will pass
-    assert add(6, 6) == 12  # Deliberate mistake to fail the test
+    assert add(6, 2) == 12  # Deliberate mistake to fail the test
