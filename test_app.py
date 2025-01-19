5c88d1f02069ad8f9667e11a1cee37430fa40ab5
Austin Neuvy <aymericneuvy@Aymerics-Laptop.local>
2025-01-19 17:33:40 +0100

    deliberate error

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5  # This will pass
-    assert add(12, 12) == 24  # Deliberate mistake to fail the test
+    assert add(12, 1) == 24  # Deliberate mistake to fail the test
