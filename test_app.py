941886dee13310b348dbd79dac8f19ed22f48e44
Austin Neuvy <aymericneuvy@aymerics-laptop.home>
2025-01-20 13:46:31 +0100

    error added

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5  # This will pass
-    assert add(12, 1) == 13  # Deliberate mistake to fail the test
+    assert add(12, 4) == 13  # Deliberate mistake to fail the test
