48a39e4d447f6c73a30a04022e07cc06a320b056
Austin Neuvy <aymericneuvy@aymerics-laptop.home>
2025-01-22 11:45:06 +0100

    error fixed

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5  # This will pass
-    assert add(1, 6) == 61  # Deliberate mistake to fail the test
+    assert add(1, 6) == 7  # Deliberate mistake to fail the test
